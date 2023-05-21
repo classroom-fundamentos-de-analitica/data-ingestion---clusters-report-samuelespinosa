6ca33925d31d1e30051bf1a6095759ccaa4521b2
samuel <saespinosab@unal.edu.co>
2023-05-21 18:24:48 -0500

    1

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,14 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
-
+import re
+from io import StringIO
 def ingest_data():
+    pattern=r"(\s*)(\d{1,5})(\s{4,})(\d{1,5})(\s{4,})(\d{1,2},\d\s*%)(\s{4,})([a-zA-Z\-\s\,\n\\(\\).\\\/]*)"
+    f=open("clusters_report.txt",'r')
+    li=list(map(list,re.findall(pattern,f.read())))
+    new_li=[['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave']]
+    for i in li: new_li.append([i[1],i[3],i[5],re.sub('\.\s+','',re.sub('\s+',' ',i[7]))])
+    return pd.DataFrame(new_li)
+ingest_data()
 
-    #
-    # Inserte su código aquí
-    #
-
-    return df
